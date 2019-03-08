d38b1ce9fa96ba17fcb0f0e0f636a300bb932cfb
David Shau <davidshau22@berkeley.edu>
2019-03-07 20:37:30 -0800

    added tf_conv

--- src/benchmarks.py
+++ src/benchmarks.py
@@ -1,7 +1,8 @@
 from timeit import Timer
 from scipy import signal
 import numpy as np
-
+import tensorflow as tf
+from network import forward
 '''
 TODOs:
 tf convolutions
@@ -20,11 +21,26 @@ Model importing/conversion
         make this import code run once, dont have it run on every forward pass
 
 '''
+model_dir = "/Users/David/Desktop/School/Launchpad/Darklight/src/model.ckpt.meta"
+import os
+cwd = os.getcwd()
+print(cwd)
+# [batch, in_height, in_width, in_channels]
+def tf_conv(mat, gaussian_blur):
+    mat = np.expand_dims(mat, axis=0)
+    mat = np.expand_dims(mat, axis=3)
+    input = tf.random_normal([1080, 720], mean=0, stddev=1)
+    input = tf.expand_dims(input, 0)
+    input = tf.expand_dims(input, 3)
+    output = forward(input)
+    with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
+        saver = tf.train.import_meta_graph(model_dir)
+        saver.restore(sess, "model.ckpt")
 
-#
-# def tf_conv(input, filter):
-#     # TODO
-#
+        out = sess.run(output, feed_dict={input : mat})
+        print(tf.shape(out))
+        return out
 
 # def cython_conv(input, filter):
 #     TODO
@@ -52,7 +68,9 @@ if __name__ == '__main__':
     test_mat = np.random.normal(size=(20, 10))
     # This is a gaussian blur kernel, I'm using it here because its relatively simple and because no components of it
     # are zero, which may (I dont know) result in misleading timeings)
-    gaussian_blur = [[1 / 16, 2 / 16, 1 / 16], [2 / 16, 4 / 16, 2 / 16], [1 / 16, 2 / 16, 1 / 16]]
+    gaussian_blur = [[1 / 16, 2 / 16, 1 / 16],
+                     [2 / 16, 4 / 16, 2 / 16],
+                     [1 / 16, 2 / 16, 1 / 16]]
 
     assert(np.allclose(scipy_conv(test_mat, gaussian_blur), np.array(naive_conv(test_mat, gaussian_blur)), atol=1e-3))
     print("functions agreed")
@@ -65,5 +83,7 @@ if __name__ == '__main__':
 
     naive_timer = Timer(lambda: naive_conv(mat, gaussian_blur))
     scipy_timer = Timer(lambda: scipy_conv(mat, gaussian_blur))
+    tf_timer = Timer(lambda: tf_conv(mat, gaussian_blur))
     print("naive fps: " + str(fps(naive_timer.timeit(number=num_times))))
     print("scipy fps: " + str(fps(scipy_timer.timeit(number=num_times))))
+    print("tf fps: " + str(fps(tf_timer.timeit(number=num_times))))
