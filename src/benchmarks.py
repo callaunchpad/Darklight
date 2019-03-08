0a058a0f98c6e4fd4cad75805761b4ede0b8343e
David Shau <davidshau22@berkeley.edu>
2019-03-07 21:51:27 -0800

    copy pasted from cchen repo

--- src/benchmarks.py
+++ src/benchmarks.py
@@ -1,8 +1,12 @@
 from timeit import Timer
 from scipy import signal
+import scipy
 import numpy as np
 import tensorflow as tf
-from network import forward
+from network import forward, pack_raw
+import glob
+import rawpy
+from PIL import Image
 '''
 TODOs:
 tf convolutions
@@ -26,21 +30,55 @@ import os
 cwd = os.getcwd()
 print(cwd)
 # [batch, in_height, in_width, in_channels]
-def tf_conv(mat, gaussian_blur):
-    mat = np.expand_dims(mat, axis=0)
-    mat = np.expand_dims(mat, axis=3)
-    input = tf.random_normal([1080, 720], mean=0, stddev=1)
-    input = tf.expand_dims(input, 0)
-    input = tf.expand_dims(input, 3)
-    output = forward(input)
+
+def tf_conv():
+
+    in_image = tf.placeholder(tf.float32, [None, None, None, 4])
+    gt_image = tf.placeholder(tf.float32, [None, None, None, 3])
+    out_image = forward(in_image)
+
     with tf.Session() as sess:
         sess.run(tf.global_variables_initializer())
         saver = tf.train.import_meta_graph(model_dir)
         saver.restore(sess, "model.ckpt")
 
-        out = sess.run(output, feed_dict={input : mat})
-        print(tf.shape(out))
-        return out
+        in_path = "./10045_00_0.1s.ARW"
+        gt_path = "./10045_00_0.04s.ARW"
+        result_dir = "./results/"
+        in_fn = os.path.basename(in_path)
+        print(in_fn)
+        gt_fn = os.path.basename(gt_path)
+        in_exposure = float(in_fn[9:-5])
+        gt_exposure = float(gt_fn[9:-5])
+        ratio = min(gt_exposure / in_exposure, 300)
+
+        raw = rawpy.imread(in_path)
+        input_full = np.expand_dims(pack_raw(raw), axis=0) * ratio
+
+        im = raw.postprocess(use_camera_wb=True, half_size=False, no_auto_bright=True, output_bps=16)
+        scale_full = np.expand_dims(np.float32(im / 65535.0), axis=0)
+
+        gt_raw = rawpy.imread(gt_path)
+        im = gt_raw.postprocess(use_camera_wb=True, half_size=False, no_auto_bright=True, output_bps=16)
+        gt_full = np.expand_dims(np.float32(im / 65535.0), axis=0)
+
+        input_full = np.minimum(input_full, 1.0)
+
+        output = sess.run(out_image, feed_dict={in_image: input_full})
+        output = np.minimum(np.maximum(output, 0), 1)
+
+        output = output[0, :, :, :]
+        gt_full = gt_full[0, :, :, :]
+        scale_full = scale_full[0, :, :, :]
+        scale_full = scale_full * np.mean(gt_full) / np.mean(
+            scale_full)  # scale the low-light image to the same mean of the groundtruth
+
+        Image.fromarray(output, 'RGB').save(
+            result_dir + 'final/%5d_00_%d_out.png' % (1, ratio))
+        Image.fromarray(scale_full, 'RGB').save(
+            result_dir + 'final/%5d_00_%d_out.png' % (2, ratio))
+        Image.fromarray(gt_full, 'RGB').save(
+            result_dir + 'final/%5d_00_%d_out.png' % (3, ratio))
 
 # def cython_conv(input, filter):
 #     TODO
@@ -83,7 +121,7 @@ if __name__ == '__main__':
 
     naive_timer = Timer(lambda: naive_conv(mat, gaussian_blur))
     scipy_timer = Timer(lambda: scipy_conv(mat, gaussian_blur))
-    tf_timer = Timer(lambda: tf_conv(mat, gaussian_blur))
+    tf_timer = Timer(lambda: tf_conv())
     print("naive fps: " + str(fps(naive_timer.timeit(number=num_times))))
     print("scipy fps: " + str(fps(scipy_timer.timeit(number=num_times))))
-    print("tf fps: " + str(fps(tf_timer.timeit(number=num_times))))
+    print("tf2 fps: " + str(fps(tf_timer.timeit(number=num_times))))
--- src/network.py
+++ src/network.py
@@ -3,6 +3,21 @@ import numpy as np
 import tensorflow.contrib.slim as slim
 import rawpy
 
+def pack_raw(raw):
+    # pack Bayer image to 4 channels
+    im = raw.raw_image_visible.astype(np.float32)
+    im = np.maximum(im - 512, 0) / (16383 - 512)  # subtract the black level
+
+    im = np.expand_dims(im, axis=2)
+    img_shape = im.shape
+    H = img_shape[0]
+    W = img_shape[1]
+
+    out = np.concatenate((im[0:H:2, 0:W:2, :],
+                          im[0:H:2, 1:W:2, :],
+                          im[1:H:2, 1:W:2, :],
+                          im[1:H:2, 0:W:2, :]), axis=2)
+    return out
 
 def lrelu(x):
     return tf.maximum(x * 0.2, x)
